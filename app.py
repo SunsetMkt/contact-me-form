6841d34e8abaaa11e2c3647e7b6aea6b7f64d9be
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2023-12-15 22:51:24 +0800

    格式化

--- app.py
+++ app.py
@@ -14,24 +14,26 @@ app = flask.Flask(__name__)
 HCAPTCHA_SITE_KEY = os.environ.get("HCAPTCHA_SITE_KEY")
 HCAPTCHA_SECRET_KEY = os.environ.get("HCAPTCHA_SECRET_KEY")
 # Yandex邮箱smtp服务器
-host_server = 'smtp.yandex.com'
+host_server = "smtp.yandex.com"
 # Yandex邮箱smtp服务器端口
-ssl_port = '465'
+ssl_port = "465"
 # 用户名
-user = os.environ.get('YANDEX_MAIL_USER')
+user = os.environ.get("YANDEX_MAIL_USER")
 # 密码（应用密码，相当于token）
-pwd = os.environ.get('YANDEX_MAIL_PWD')
+pwd = os.environ.get("YANDEX_MAIL_PWD")
 # 发件人的邮箱
-sender_mail = os.environ.get('YANDEX_MAIL_USER')
+sender_mail = os.environ.get("YANDEX_MAIL_USER")
 # 收件人
-receiver = os.environ.get('MAIL_RECEIVER')
+receiver = os.environ.get("MAIL_RECEIVER")
 
 
 # CORS
 # flask_cors.CORS(app, resources={r"/*": {"origins": "*"}})
 # Allow *.lwd-temp.* and *.lwd-temp.*:port
-flask_cors.CORS(app, resources={
-                r"/*": {"origins": r"^(https?://)?(\w+\.)?(lwd-temp)\.?(\w+)?(:\d+)?$"}})
+flask_cors.CORS(
+    app,
+    resources={r"/*": {"origins": r"^(https?://)?(\w+\.)?(lwd-temp)\.?(\w+)?(:\d+)?$"}},
+)
 
 
 def get_ip():
@@ -40,11 +42,11 @@ def get_ip():
     # cf-connecting-ip x-real-ip
     # x-forwarded-for
     # Then from request
-    ip = flask.request.headers.get('cf-connecting-ip')
+    ip = flask.request.headers.get("cf-connecting-ip")
     if ip is None:
-        ip = flask.request.headers.get('x-real-ip')
+        ip = flask.request.headers.get("x-real-ip")
     if ip is None:
-        ip = flask.request.headers.get('x-forwarded-for')
+        ip = flask.request.headers.get("x-forwarded-for")
     if ip is None:
         ip = flask.request.remote_addr
     return ip
@@ -71,19 +73,19 @@ def get_request_info():
 
 def NoneTypeHandler(obj):
     if obj is None:
-        return ''
+        return ""
     else:
         return obj
 
 
-def post_message_to_endpoint(message, remote_ip='Unknown', info='', frontendappend=''):
+def post_message_to_endpoint(message, remote_ip="Unknown", info="", frontendappend=""):
     # Append IP and useragent to message
     message += "\n\nIP: " + NoneTypeHandler(remote_ip)
     message += "\nRequestInfo: " + NoneTypeHandler(info)
     message += "\nFrontendAppend: " + NoneTypeHandler(frontendappend)
 
-    msg = MIMEText(message, "plain", 'utf-8')
-    msg["Subject"] = Header(f"Contact Me Form from {remote_ip}", 'utf-8')
+    msg = MIMEText(message, "plain", "utf-8")
+    msg["Subject"] = Header(f"Contact Me Form from {remote_ip}", "utf-8")
     msg["From"] = sender_mail
     msg["To"] = receiver
     try:
@@ -99,19 +101,19 @@ def post_message_to_endpoint(message, remote_ip='Unknown', info='', frontendappe
 
 
 # Handle / (index)
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
     # return template('form.html')
-    return flask.render_template('form.html', site_key=HCAPTCHA_SITE_KEY)
+    return flask.render_template("form.html", site_key=HCAPTCHA_SITE_KEY)
 
 
 # Handle /submit
-@app.route('/submit', methods=["POST"])
+@app.route("/submit", methods=["POST"])
 def submit_endpoint():
     # Get form data
     data = flask.request.form
-    message = data.get('message')
-    frontendappend = data.get('frontendappend')
+    message = data.get("message")
+    frontendappend = data.get("frontendappend")
 
     remote_ip = NoneTypeHandler(get_ip())
 
@@ -119,32 +121,38 @@ def submit_endpoint():
     info = get_request_info()
 
     # Verify
-    response = data.get('h-captcha-response')
+    response = data.get("h-captcha-response")
     VERIFY_URL = "https://hcaptcha.com/siteverify"
-    r = requests.post(VERIFY_URL, data={
-        "secret": HCAPTCHA_SECRET_KEY,
-        "response": response,
-        "remoteip": remote_ip,
-        "sitekey": HCAPTCHA_SITE_KEY
-    })
+    r = requests.post(
+        VERIFY_URL,
+        data={
+            "secret": HCAPTCHA_SECRET_KEY,
+            "response": response,
+            "remoteip": remote_ip,
+            "sitekey": HCAPTCHA_SITE_KEY,
+        },
+    )
     response_json = r.json()
     success = response_json["success"]
     try:
         if success:
             if post_message_to_endpoint(message, remote_ip, info, frontendappend):
-                return flask.render_template('success.html', message=message)
+                return flask.render_template("success.html", message=message)
             else:
                 message = "It appears there might be an issue on our end. We apologize for the inconvenience."
-                return flask.render_template('deny.html', message=message)
+                return flask.render_template("deny.html", message=message)
         else:
             if "error-codes" in response_json:
-                message = "We've encountered an hCaptcha error: " + response_json["error-codes"][0]
+                message = (
+                    "We've encountered an hCaptcha error: "
+                    + response_json["error-codes"][0]
+                )
             else:
                 message = "We've encountered an hCaptcha error. Kindly ensure that you have successfully solved the captcha before proceeding."
-            return flask.render_template('deny.html', message=message)
+            return flask.render_template("deny.html", message=message)
     except:
         message = "It appears there might be an issue on our end. We apologize for the inconvenience."
-        return flask.render_template('deny.html', message=message)
+        return flask.render_template("deny.html", message=message)
 
 
 if __name__ == "__main__":
